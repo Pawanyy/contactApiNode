46ded6efe384cd1a6361a34999c7295c9b787128
Pawanyy <pawanyyy01@gmail.com>
2023-11-17 02:10:08 +0530

    simplified the code

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -9,12 +9,6 @@ import {
 
 export const router = express.Router();
 
-router.route("/").get(getContacts);
+router.route("/").get(getContacts).post(createContact);
 
-router.route("/").post(createContact);
-
-router.route("/:id").get(getContact);
-
-router.route("/:id").put(updateContact);
-
-router.route("/:id").delete(deleteContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
