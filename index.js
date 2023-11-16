76e04f11f556c77cf7a9b720443b2bfd5f19b5e0
Pawanyy <pawanyyy01@gmail.com>
2023-11-17 01:57:23 +0530

    added express use json

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   return res.status(200).json({ message: "Welcome to the root API!" });
 });
